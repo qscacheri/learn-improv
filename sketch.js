e7fed484a6566ce25bd719bb87b1aea44c10272d
Quin Scacheri <qs366@nyu.edu>
2018-11-27 14:04:17 -0500

    updated sidebar

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 var sketch = function (p) {
 
 	var addBtn = document.getElementById("add");
-
+	var chordForm = document.getElementById("form");
+	var beatForm = document.getElementById("beat-form");
 
 	var baseUrl = "https://qscacheri.github.io/Sound-Samples/MusyngKite/";
 	var sampler = new Tone.Sampler({
@@ -59,9 +60,188 @@ var sketch = function (p) {
 		"Bb4": baseUrl + "acoustic_grand_piano" + "-mp3/" + "Bb4.mp3",
 		"B4": baseUrl + "acoustic_grand_piano" + "-mp3/" + "B4.mp3"
 	})
+
+
+	Tone.Buffer.on('load', function () {
+		console.log('...done');
+		sampler.sync();
+		sampler.toMaster();
+
+
+	})
+
+	Tone.Buffer.on('progress', function () {
+		console.log('loading...');
+	})
+
+	Tone.Buffer.on('error', function () {
+		console.log('error');
+	})
+
+	function noteToNum(note) {
+
+
+		var noteNum = 0;
+
+		if (note == "C")
+			noteNum = 0;
+		else if (note == "C#/Db")
+			noteNum = 1;
+		else if (note == "D")
+			noteNum = 2;
+		else if (note == "D#/Eb")
+			noteNum = 3;
+		else if (note == "E")
+			noteNum = 4;
+		else if (note == "F")
+			noteNum = 5;
+		else if (note == "F#/Gb")
+			noteNum = 6;
+		else if (note == "G")
+			noteNum = 7;
+		else if (note == "G#/Ab")
+			noteNum = 8;
+		else if (note == "A")
+			noteNum = 9;
+		else if (note == "A#/Bb")
+			noteNum = 10;
+		else if (note == "B")
+			noteNum = 11;
+		return noteNum;
+	}
+
+	function numToNote(note) {
+
+		return notesAndNums[note];
+	}
+
+	function convertBeat(beats) {
+		return beatConvert[beats];
+
+	}
+
+
+	function findThird(root, q) {
+		//            console.log(root + "root");
+		var mod;
+		if (q == ("major")) {
+			mod = 0;
+		} else if (q == ("minor")) {
+			mod = -1;
+		} else if (q == ("diminished")) {
+			mod = -1;
+		} else if (q == ("augmented")) {
+			mod = 0;
+		}
+
+		return (root + 4 + mod);
+	}
+
+	function findFifth(root, q) {
+		var mod;
+		if (q == ("major")) {
+			mod = 0;
+		} else if (q == ("minor")) {
+			mod = 0;
+		} else if (q == ("diminished")) {
+			mod = -1;
+		} else if (q == ("augmented")) {
+			mod = 1;
+		}
+
+		return (root + mod + 7);
+	}
+
+	function makeChord(q, r) {
+
+		var root = noteToNum(r);
+		var notes = [];
+		notes[0] = root + (12 * 5);
+		notes[1] = findThird(root, q) + (12 * 5);
+		notes[2] = findFifth(root, q) + (12 * 5);
+		var noteAndTime;
+
+		for (var i = 0; i < 3; i++) {
+			noteAndTime = {
+				note: Tone.Frequency(notes[i], "midi").toNote(),
+				beats: lastTime
+			};
+
+			sched.push(noteAndTime);
+			console.log(noteAndTime.note);
+		}
+		lastTime += parseFloat(beatBox.value);
+		totalNotes += 3;
+
+		if (lastTime % 4 != 0) {
+			maxMeasures = Math.floor(lastTime / 4) + 1;
+		} else(maxMeasures = lastTime / 4);
+		console.log(maxMeasures + '= max measures');
+
+		Tone.Transport.setLoopPoints(0, maxMeasures + 'm');
+	};
+
+	function loopSwitch() {
+		loopStatus = !loopStatus;
+		console.log(sampler.loaded);
+	}
+
+	function pause() {
+		Tone.Transport.stop();
+
+	}
+
+	function playNote() {
+		var measure;
+		var beat;
+		//            Tone.Transport.setLoopPoints(0, "2m");
+		Tone.Transport.loop = true;
+		Tone.Transport.start();
+
+		for (var i = 0; i < totalNotes; i++) {
+
+			if (sched[i].beats % 4 == 0) {
+				measure = sched[i].beats / 4 + 'm';
+				sampler.triggerAttackRelease(sched[i].note, '8n', measure);
+			} else {
+				measure = Math.floor(sched[i].beats / 4);
+				beat = sched[i].beats % 4;
+				console.log('m = ' + measure);
+				console.log('n = ' + beat);
+				sampler.triggerAttackRelease(sched[i].note, '8n', Tone.TimeBase(beat, 'n') + Tone.TimeBase(measure, 'm'));
+			}
+		}
+
+
+		tempo = tempoBox.value;
+		Tone.Transport.bpm.value = tempo;
+
+		beatTime = 60 / tempo;
+		barTime = beatTime * 4;
+
+		totalTime = lastTime * beatTime;
+
+	}
+
+	function addChord() {
+		beatTime = 60 / tempo;
+		barTime = beatTime * 4;
+		quality = qualList.options[qualList.selectedIndex].value;
+		root = rootList.options[rootList.selectedIndex].value;
+		makeChord(quality, root);
+		chordList.push(root + " " + quality);
+	}
+
+	function reset() {
+		sched = [];
+		chordList = [];
+	}
+
+
+
 	var width = window.innerWidth;
 	var height = window.innerWidth;
-	var blockWidth = window.innerWidth / 8;
+	var blockWidth = window.innerWidth / 12;
 	var blockHeight = 50;
 	var blockArray = [];
 	var blockEnd = {
@@ -71,7 +251,6 @@ var sketch = function (p) {
 
 
 	p.setup = function () {
-		console.log(width / 8);
 		p.createCanvas((window.innerWidth) - width / 8 - 15, (window.innerHeight / 8) * 8);
 		p.background(100);
 		p.colorMode(p.HSL, 360, 100, 100);
@@ -127,9 +306,12 @@ var sketch = function (p) {
 		p.text(block.text + " x " + block.duration + " beats", block.x + (block.width / 2), block.y + (block.height / 2));
 	}
 
+
 	addBtn.addEventListener("click", function () {
-		addBlock(document.getElementById("form").value, 1);
-		document.getElementById("form").value = "";
+		addBlock(chordForm.value, parseInt(beatForm.value));
+		chordForm.value = "";
+		beatForm.value = "";
+		addChord()
 	});
 
 
